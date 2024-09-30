e31d2e254c87b6059c18075aa66fca02bcd68b32
Jay_Tech <agananoadeyemi001@gmail.com>
2024-09-30 14:24:53 +0100

    made little changes

--- src/main/java/com/jaytech/keycloak/JwtAuthConverter.java
+++ src/main/java/com/jaytech/keycloak/JwtAuthConverter.java
@@ -45,27 +45,29 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
 
     private String getPrincipleClaimName(Jwt jwt) {
         String claimName = JwtClaimNames.SUB;
-        if (principleAttribute != null){
+        if (principleAttribute != null) {
             claimName = principleAttribute;
         }
         return jwt.getClaim(claimName);
     }
 
-    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt){
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
         Map<String, Object> resourceAccess;
         Map<String, Object> resource;
         Collection<String> resourceRoles;
-        if (jwt.getClaim("resource_access") == null){
+        if (jwt.getClaim("resource_access") == null) {
             return Set.of();
         }
         resourceAccess = jwt.getClaim("resource_access");
-        if (resourceAccess.get(resourceId) == null){
+
+        if (resourceAccess.get(resourceId) == null) {
             return Set.of();
         }
         resource = (Map<String, Object>) resourceAccess.get(resourceId);
 
         resourceRoles = (Collection<String>) resource.get("roles");
-        return resourceRoles.stream()
+        return resourceRoles
+                .stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
                 .collect(Collectors.toSet());
     }
--- src/main/java/com/jaytech/keycloak/SecurityConfig.java
+++ src/main/java/com/jaytech/keycloak/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.jaytech.keycloak;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,7 +14,10 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -26,7 +30,8 @@ public class SecurityConfig {
 
         http
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
 
         http
                 .sessionManagement()
